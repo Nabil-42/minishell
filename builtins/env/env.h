356d67fdf7673cd3461e3433e948a19cacaff045
nabil <abboud@outlook.com>
2024-06-18 23:53:55 +0200

    ménage

--- builtins/env/env.h
+++ builtins/env/env.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   env.h                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nabboud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: nabil <nabil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/11 14:51:24 by tissad            #+#    #+#             */
-/*   Updated: 2024/06/13 15:55:03 by nabboud          ###   ########.fr       */
+/*   Updated: 2024/06/18 17:39:17 by nabil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 # define ENV_H
 
 #include <libft.h>
-#include "../lib/libft/includes/libft.h"
+#include "../../lib/libft/includes/libft.h"
 
 # define MAX_ENV 512
 
@@ -25,6 +25,17 @@ typedef struct s_env
     int     nb_var;
 }   t_env;
 
+unsigned int hash_function(const char *key, unsigned int table_size);
+char	*get_key(char *env_var);
+void	dup_env(t_env *local_env, char **envp);
+void	del(void *content);
+char **get_local_env(t_env *env);
+void	init_local_env(t_env *local_env, char **envp);
+void	ft_env(t_env *env);
+void	ft_unset(t_env *env, char *key);
+void	ft_export(t_env *env, char *env_var);
+int	exist_env_var(char *env_var, char *key, t_list *lst);
+void	delete_envp(t_env *env);
 void	ft_env(t_env *env);
 void	ft_export(t_env *env, char *env_var);
 void	init_local_env(t_env *local_env, char **envp);
